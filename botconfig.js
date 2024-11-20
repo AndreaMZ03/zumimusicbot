9994b15ac968d0cf675f1a2944161cfcffababd4
AndreaMZ03 <131559159+AndreaMZ03@users.noreply.github.com>
2024-11-20 17:24:27 +0100

    Add files via upload

---
+++ botconfig.js
@@ -0,0 +1,45 @@
+module.exports = {
+  Admins: ["UserID", "UserID"], // Admins of the bot, I don't know what this do. -Darren.
+  ExpressServer: true, // If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || "!", // Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://www.youtube.com/watch?v=rS94ibzYCJY", // Support Server Link
+  Token: process.env.Token || "MTA0NjQ4NTA0MDE1MTkyNDg3OA.GaJfZw.uCeF6knKhFB9aE1ukGAsK5fXSbbaxNWUKT0F5Y", // Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "1046485040151924878", // Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "5HbyN8k4wX8LymP3m8vatsROuGfBzPg9", // Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
+  ServerDeafen: true, // If you want bot to stay deafened
+  DefaultVolume: 50, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
+  CallbackURL: "/api/callback", // Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
+  "24/7": false, // Make the bot stays in VC 24/7 (when you reboot the bot will **not** automatically rejoin.)
+  CookieSecret: "Pikachu is not cute", // A cookie for you, cookie for me. make sure you change this value!
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", // URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  EmbedColor: "RANDOM", // Color of most embeds | Custom Hex value are supported. I.e: "#36393F"
+  Permissions: 2205281600, // Bot Inviting Permissions
+  Website: process.env.Website || "https://example.com", // Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
+  // If you get invalid oauth, make sure on the discord developer page you set the oauth url to something like: https://example.com/api/callback.
+
+  Presence: {
+    status: "online", // You can show online, idle, and dnd
+    name: "música hacendado", // The message shown
+    type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+  },
+
+  // You need a lavalink server for this bot to work!!!!
+  // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
+  Lavalink: {
+    id: "Main", //- Used for indentifier. You can set this to whatever you want.
+    host: "lava.link", //- The host name or IP of the lavalink server.
+    port: 80, // The port that lavalink is listening to. This must be a number!
+    pass: "youshallnotpass", //- The password of the lavalink server.
+    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
+    retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
+    retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
+  },
+  // Spotify Integration, allows you to enter a spotify link.
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "6022186cfa2c470bab967c8339c7870f", // Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "32aa50d9ef394501acb51fd41a1b9342", // Spotify Client Secret
+  },
+};
